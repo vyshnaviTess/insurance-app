d8341a8a7894039ba48b1a67636866e36deb5eb3
vyshnaviTess <vaishuekshitha@gmail.com>
2025-09-15 07:45:03 +0100

    createPolicy usecase
    - removed direct dispatch of actions and make use of createPolicy in new.tsx
    - deleted unneccessary deletePolicy.tsx screen as i'm handing with simple confirmDelete with alert

--- app/policy/new.tsx
+++ app/policy/new.tsx
@@ -2,34 +2,35 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
 import { useRouter } from "expo-router";
-import { policiesActions } from "../../src/store/policiesSlice";
 import { Screen } from "../../src/ui/components/Screen";
 import { TextField } from "../../src/ui/components/TextField";
 import { Button } from "../../src/ui/components/Button";
 import { Select } from "../../src/ui/components/Select"; // 👈 new import
+import { createPolicy } from "@/domain/usecases/createPolicy";
+import { AppDispatch } from "@/store";
 
 export default function NewPolicy() {
   const [provider, setProvider] = useState("");
   const [type, setType] = useState<"car" | "van" | "motorbike" | "house">("car"); // 👈 new state
-  const dispatch = useDispatch();
+   const dispatch = useDispatch<AppDispatch>();
   const router = useRouter();
 
   function handleSave() {
     const id = nanoid();
-    dispatch(
-      policiesActions.upsertPolicy({
-        id,
-        type, // 👈 now dynamic
-        provider,
-        policyNumber: "ABC-123",
-        startDate: new Date().toISOString(),
-        endDate: new Date(Date.now() + 86400 * 1000 * 100).toISOString(),
-        premium: 500,
-        coverage: {},
-        documents: [],
-        reminders: [],
-      })
-    );
+    const newPolicy = {
+    id,
+    type,
+    provider,
+    policyNumber: "ABC-123",
+    startDate: new Date().toISOString(),
+    endDate: new Date(Date.now() + 86400 * 1000 * 100).toISOString(),
+    premium: 500,
+    coverage: {},
+    documents: [],
+    reminders: [],
+  };
+
+  dispatch(createPolicy(newPolicy));
     router.replace(`/policy/${id}`);
   }
 

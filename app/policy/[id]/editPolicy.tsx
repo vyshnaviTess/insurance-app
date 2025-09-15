b2b8f42008bb59a05fc13623bc3e69d9ad81d793
vyshnaviTess <vaishuekshitha@gmail.com>
2025-09-15 07:51:43 +0100

    Update editPolicy.tsx
    - updated the use of updatePolicy instead of directly policiesactions

--- app/policy/[id]/editPolicy.tsx
+++ app/policy/[id]/editPolicy.tsx
@@ -2,22 +2,24 @@ import { useState } from "react";
 import { Text } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useDispatch, useSelector } from "react-redux";
-import { policiesSelectors, policiesActions } from "@/store/policiesSlice";
+import { policiesSelectors } from "@/store/policiesSlice";
 import { Screen } from "@/ui/components/Screen";
 import { TextField } from "@/ui/components/TextField";
 import { Button } from "@/ui/components/Button";
+import { updatePolicy } from "@/domain/usecases/updatePolicy";
+import { AppDispatch } from "@/store";
 
 export default function EditPolicy() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const policy = useSelector((s: any) => policiesSelectors.selectById(s, id!));
   const [provider, setProvider] = useState(policy?.provider ?? "");
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   const router = useRouter();
 
   if (!policy) return <Screen title="Edit Policy"><Text>Not found</Text></Screen>;
 
   function handleSave() {
-    dispatch(policiesActions.upsertPolicy({ ...policy, provider }));
+   dispatch(updatePolicy({ ...policy, provider })); 
     router.back();
   }
 
